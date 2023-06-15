205d202848b84468c7259be1033f20eb85dc717f
FtimaBen <75008648+FtimaBen@users.noreply.github.com>
2023-06-15 23:44:05 +0100

    Delete form.py

--- form.py
+++
@@ -1,7 +0,0 @@
-from wtforms import StringField, SubmitField, URLField
-from wtforms.validators import DataRequired, Length
-from flask_wtf import FlaskForm
-
-class ProductForm(FlaskForm):
-    url = URLField(label='Product Url', validators=[DataRequired()])
-    submit = SubmitField('Submit')
